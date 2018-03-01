fb189d8dcdac80fa174599db5936ced2c72513ce
xin3w <xin3w@126.com>
2018-03-01 23:02:59 +0800

    切换自动获取input焦点

--- vue-cli/src/router/index.js
+++ vue-cli/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import TestFocus from '@/components/TestFocus'
 
 Vue.use(Router)
 
@@ -8,8 +9,7 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: TestFocus
     }
   ]
 })
