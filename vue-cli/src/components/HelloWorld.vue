b58aa65789c75afbeaaada16f4e0ef80cdc19548
xin3w <xin3w@126.com>
2018-03-18 01:41:09 +0800

    1

--- vue-cli/src/components/HelloWorld.vue
+++ vue-cli/src/components/HelloWorld.vue
@@ -1,26 +1,31 @@
 <template>
   <div>
     {{dataUrl}}
+    <mt-button plain>plain</mt-button>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      dataUrl:''
+  import {
+    MessageBox
+  } from 'mint-ui';
+  export default {
+    name: 'HelloWorld',
+    data() {
+      return {
+        msg: 'Welcome to Your Vue.js App',
+        dataUrl: ''
+      }
+    },
+    mounted() {
+      this.$axios("http://ltc-bj-dev.c6396418dd7954b228d8b967c8a929103.cn-beijing.alicontainer.com/api/public/v1/url").then(res => {
+        this.dataUrl = res;
+      })
     }
-  },
-  mounted(){
-    this.$axios("http://ltc-bj-dev.c6396418dd7954b228d8b967c8a929103.cn-beijing.alicontainer.com/api/public/v1/url").then(res=>{
-      this.dataUrl=res;
-    })
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+
 </style>
--- vue-cli/src/main.js
+++ vue-cli/src/main.js
@@ -3,10 +3,14 @@ import App from '@/App'
 import axios from 'axios'
 import router from '@/router'
 import store from '@/store'
+import MintUI from 'mint-ui'
+import 'mint-ui/lib/style.css'
+Vue.use(MintUI)
 
 Vue.config.productionTip = false
 
 Vue.prototype.$axios = axios
+
 
 window.Vue = new Vue({
   el: '#app',
